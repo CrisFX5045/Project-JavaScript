c0f8c714d60d76d1019b04e8edf609753399327e
xmar08 <xmar08@gmail.com>
2020-10-14 12:50:52 -0600

    correciones
    hoy

--- FrontEnd/JS/main50Mejores.js
+++ FrontEnd/JS/main50Mejores.js
@@ -1,12 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
 function mobMenuShow(){
     let menu = document.body.getElementsByClassName('topNav')[0];
     menu.style.right = "0%";
 }
-
-AOS.init();
-
 function mobMenuClose(){
     let menu = document.body.getElementsByClassName('topNav')[0];
     menu.style.right = "-50%";
 }
-
