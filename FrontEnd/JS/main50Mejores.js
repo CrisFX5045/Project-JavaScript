ce2dc9ce76de36830c86b58a1f5568839f45b575
CrisFX5045 <70766389+CrisFX5045@users.noreply.github.com>
2020-10-14 01:39:22 -0600

    Animacion java scripth
    Animacion java scripth

--- FrontEnd/JS/main50Mejores.js
+++ FrontEnd/JS/main50Mejores.js
@@ -1,19 +1,12 @@
-
-
-
-
-
-
-
-
-
-
-
 function mobMenuShow(){
     let menu = document.body.getElementsByClassName('topNav')[0];
     menu.style.right = "0%";
 }
+
+AOS.init();
+
 function mobMenuClose(){
     let menu = document.body.getElementsByClassName('topNav')[0];
     menu.style.right = "-50%";
 }
+
--- FrontEnd/JS/mainGaleria.js
+++ FrontEnd/JS/mainGaleria.js
@@ -13,6 +13,7 @@ function mobMenuShow(){
     let menu = document.body.getElementsByClassName('topNav')[0];
     menu.style.right = "0%";
 }
+AOS.init();
 function mobMenuClose(){
     let menu = document.body.getElementsByClassName('topNav')[0];
     menu.style.right = "-50%";
