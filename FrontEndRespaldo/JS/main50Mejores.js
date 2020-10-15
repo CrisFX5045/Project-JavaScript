4ef747c13cb782b1bcc73e50d2918e7e383c5022
Nayeeb <jnayeebjnrf@gmail.com>
2020-10-15 12:06:11 -0600

    Descripcion fotos y mobile

--- FrontEndRespaldo/JS/main50Mejores.js
+++ FrontEndRespaldo/JS/main50Mejores.js
@@ -13,4 +13,22 @@ function mobMenuClose(){
 
 function buscador(){
     document.getElementById("busca").value = "";
+} 
+function openDescription(){
+    let descrip = document.body.getElementsByClassName('description')[0]
+    descrip.style.zIndex = "99";
+    descrip.style.opacity = "100%";
+
+    let closeBtn = document.body.
+    getElementsByClassName('descripClose')[0]
+    closeBtn.style.zIndex = "99";
+    closeBtn.style.opacity = "100%";
+}
+function closeDescrip(){
+    let descrip = document.body.getElementsByClassName('description')[0]
+    descrip.style.zIndex = "0";
+    descrip.style.opacity = "0%";
+    let closeBtn = document.body.getElementsByClassName('descripClose')[0]
+    closeBtn.style.zIndex = "0";
+    closeBtn.style.opacity = "0%";
 }
