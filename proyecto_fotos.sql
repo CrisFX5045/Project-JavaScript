93b44aea025e6e8e7b0b8919a304964a3a69329a
Nayeeb <jnayeebjnrf@gmail.com>
2020-11-15 16:16:33 -0600

    database

---
+++ proyecto_fotos.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-11-2020 a las 23:15:27
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_fotos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotos`
+--
+
+CREATE TABLE `fotos` (
+  `id_fotos` int(10) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `categoria` enum('naturaleza','aerea','paisaje','personas') NOT NULL,
+  `votos` int(10) NOT NULL,
+  `estado` enum('pendiente','denegado','aprobado') NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `publication_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_users`
+--
+
+CREATE TABLE `tb_users` (
+  `username` varchar(10) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `registered_at` datetime NOT NULL,
+  `last_login_at` datetime NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `full_name` varchar(200) NOT NULL,
+  `tipo_user` enum('admin','cliente') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`id_fotos`);
+
+--
+-- Indices de la tabla `tb_users`
+--
+ALTER TABLE `tb_users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `id_fotos` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_users`
+--
+ALTER TABLE `tb_users`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
