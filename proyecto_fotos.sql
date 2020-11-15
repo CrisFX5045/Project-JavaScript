a59abf3f76136ec8cc030b85e3e10cc232362670
Nayeeb <jnayeebjnrf@gmail.com>
2020-11-15 16:30:16 -0600

    Update proyecto_fotos.sql

--- proyecto_fotos.sql
+++ proyecto_fotos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-11-2020 a las 23:15:27
+-- Tiempo de generación: 15-11-2020 a las 23:29:35
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyecto_fotos`
 --
+CREATE DATABASE IF NOT EXISTS `proyecto_fotos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `proyecto_fotos`;
 
 -- --------------------------------------------------------
 
